d0fa8ed7238b76e75457395e9eeefa0ce1737603
Aryan Anand <aryananand.syn@gmail.com>
2023-06-11 08:49:24 +0530

    version 1.0.0.2

--- app.py
+++ app.py
@@ -10,11 +10,9 @@ app = Flask(__name__)
 
 loaded_model = pickle.load(open("model.pkl", 'rb'))
 
-
 @app.route('/')
 def home():
     return render_template('home.html')
-
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -31,19 +29,17 @@ def predict():
 
     prediction = loaded_model.predict(single_pred)
 
-    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange",
-                 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
-                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans",
-                 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
+    crop_dict = {1: "Rice", 2: "Maize", 3: "Jute", 4: "Cotton", 5: "Coconut", 6: "Papaya", 7: "Orange", 8: "Apple", 9: "Muskmelon", 10: "Watermelon", 11: "Grapes", 12: "Mango", 13: "Banana",
+                 14: "Pomegranate", 15: "Lentil", 16: "Blackgram", 17: "Mungbean", 18: "Mothbeans", 19: "Pigeonpeas", 20: "Kidneybeans", 21: "Chickpea", 22: "Coffee"}
 
     if prediction[0] in crop_dict:
         crop = crop_dict[prediction[0]]
         result = "{} is the most suitable crop as per the input data.".format(crop)
+        
     else:
         result = "Oops! The crop cannot be determined with the input data."
 
     return render_template('home.html', prediction=result)
-
 
 if __name__ == '__main__':
     app.run(debug=True)
